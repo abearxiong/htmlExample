db449a5b8b43ced99f4713384e7bf677f7f6cd45
abearxiong bd <xiongxiao1012@outlook.com>
2020-01-16 17:55:53 +0800

    ace and prismjs,hightlightjs

--- 2001-0001 CSS 旋转的光圈/setCode.js
+++ 2001-0001 CSS 旋转的光圈/setCode.js
@@ -1,5 +1,6 @@
 // 获取除开node_modules的文件列表
 import fs from "fs";
+
 let filesPath = ".";
 
 let getNeedFiles = [];
@@ -22,31 +23,27 @@ getFiles.forEach(item => {
 console.log("获取的列表", getNeedFiles);
 
 let writeUrl = getNeedFiles.map(item => {
-  // return `<a class="example-list" target="__blank" href="./${item}/dist/index.html">${item}</a>`;
-  return `<a class="example-list" target="_blank" href="./${item}/dist/index.html">${item}</a>`;
+  return `<a class="example-list" href="./${item}/dist/index.html">${item}</a>`;
 });
 writeUrl = writeUrl.join(" ");
 let html = `
-<!DOCTYPE html>
-<html lang="zh-cn">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>我的例子</title>
-    <style>
-    .example-list{
-        display:block;
-    }
-    body{
-        width: 80%;
-        margin: auto;
-    }
-    </style>
-</head>
-<body>
+<html>
+    <head>
+        <title>我的例子的列表</title>
+        <meta charset="utf-8">
+        <style>
+        .example-list{
+            display:block;
+        }
+        body{
+            width: 80%;
+            margin: auto;
+        }
+        </style>
+    </head>
+    <body>
     ${writeUrl}
-</body>
+    </body>
 </html>
 `;
 fs.writeFileSync("index.html", html);
